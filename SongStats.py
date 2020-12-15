7921f75c3d3c73604c3656eb5900cb145895c36f
William Jenkins <60557149+RN89890@users.noreply.github.com>
2020-12-14 21:26:02 -0500

    Add files via upload

---
+++ SongStats.py
@@ -0,0 +1,74 @@
+import csv
+
+with open('new_output.csv', 'r') as InputFile:
+    FileReader = csv.DictReader(InputFile)
+    TitleList = []
+    ArtistList = []
+    LyricList = []
+    GenreList = []
+
+    # title, artist, lyrics, genre
+    for row in FileReader:
+        TitleList.append(row['title'])
+        ArtistList.append(row['artist'])
+        LyricList.append(row['lyrics'][0:(len(row['lyrics']) - 1)].replace("\n", ""))
+        GenreList.append(row['genre'])
+
+    # Output totals.
+    print("Total song titles: " + str(len(TitleList)))
+    print("Total artists: " + str(len(ArtistList)))
+    print("Total genres: " + str(len(GenreList)))
+
+    # Output unique totals.
+    print("\nTotal unique song titles: " + str(len(set(TitleList))))
+    print("Total unique artists: " + str(len(set(ArtistList))))
+    print("Total unique genres: " + str(len(set(GenreList))) + "\n")
+
+    # Count number of songs per genre.
+    for genre in set(GenreList):
+        print("Total songs in " + str(genre) + " genre: " + str(GenreList.count(genre)))
+
+        GenreTokens = []
+
+        for g in range(len(GenreList)):
+            if GenreList[g] == genre:
+                for t in LyricList[g].split(" "):
+                    GenreTokens.append(t)
+
+        print("Total tokens: " + str(len(GenreTokens)))
+        print("Total types: " + str(len(set(GenreTokens))))
+        print("")
+
+    # Count number of tokens
+    tokens = []
+
+    for song in LyricList:
+        pieces = song.split(" ")
+
+        for piece in pieces:
+            tokens.append(piece)
+
+    print("\nTotal number of tokens: " + str(len(tokens)))
+
+    # Count number of types
+    types = {}
+
+    for t in tokens:
+        if t in types:
+            types[t] = types[t] + 1
+        else:
+            types[t] = 1
+
+    print("Total number of types: " + str(len(types)))
+
+    # Output average number of words per song.
+    print("Average number of words per song: " + str(len(tokens) / len(TitleList)) + "\n")
+
+    # Output top ten types.
+    stypes = sorted(types, key=types.get, reverse=True)
+
+    print("Top ten types: ")
+    print("\tType\tCount")
+
+    for i in range(10):
+        print(str(i + 1) + "\t" + str(stypes[i]) + "\t" + str(types[stypes[i]]))
