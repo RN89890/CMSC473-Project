874046ff289a3390737b5db24e5bed4c6a0f3094
William Jenkins <60557149+RN89890@users.noreply.github.com>
2020-12-14 21:27:05 -0500

    Update SongStats.py

--- SongStats.py
+++ SongStats.py
@@ -72,3 +72,91 @@ with open('new_output.csv', 'r') as InputFile:
 
     for i in range(10):
         print(str(i + 1) + "\t" + str(stypes[i]) + "\t" + str(types[stypes[i]]))
+        
+"""
+Total song titles: 1158
+Total artists: 1158
+Total genres: 1158
+
+Total unique song titles: 1149
+Total unique artists: 1007
+Total unique genres: 15
+
+Total songs in country genre: 48
+Total tokens: 5083
+Total types: 1607
+
+Total songs in folk genre: 29
+Total tokens: 2751
+Total types: 1150
+
+Total songs in new age genre: 3
+Total tokens: 230
+Total types: 133
+
+Total songs in blues genre: 15
+Total tokens: 1498
+Total types: 596
+
+Total songs in jazz genre: 35
+Total tokens: 2890
+Total types: 1059
+
+Total songs in rap genre: 52
+Total tokens: 14566
+Total types: 5092
+
+Total songs in latin genre: 18
+Total tokens: 1602
+Total types: 853
+
+Total songs in reggae genre: 18
+Total tokens: 3593
+Total types: 1234
+
+Total songs in metal genre: 88
+Total tokens: 7816
+Total types: 2848
+
+Total songs in rock genre: 539
+Total tokens: 53015
+Total types: 8261
+
+Total songs in electronic genre: 66
+Total tokens: 6013
+Total types: 1818
+
+Total songs in pop genre: 160
+Total tokens: 21850
+Total types: 4305
+
+Total songs in rnb genre: 52
+Total tokens: 7841
+Total types: 1801
+
+Total songs in world genre: 5
+Total tokens: 691
+Total types: 368
+
+Total songs in punk genre: 30
+Total tokens: 2696
+Total types: 1185
+
+
+Total number of tokens: 132135
+Total number of types: 17089
+Average number of words per song: 114.10621761658031
+
+Top ten types:
+        Type    Count
+1       im      1716
+2       love    1369
+3       know    1277
+4       like    1104
+5       get     869
+6       got     840
+7       oh      816
+8       never   751
+9       one     716
+10      see     698
+"""
